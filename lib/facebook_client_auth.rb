cf7a2417e71cac004af415df38e5fb9a72a99a95
ajet <ravi.teja@navyuginfo.com>
2013-11-05 12:56:27 +0530

    First commit to master

---
+++ lib/facebook_client_auth.rb
@@ -0,0 +1,7 @@
+require "facebook_client_auth/version"
+
+module FacebookClientAuth
+  def self.hi
+		"Hi welcome to the FacebookClientAuth"
+  end
+end
---
+++ lib/facebook_client_auth/version.rb
@@ -0,0 +1,3 @@
+module FacebookClientAuth
+  VERSION = "0.0.1"
+end
